a80df6ca333f8dc3bdd1d5b170da4b20f180317c
raj <raj0042gautam@gmail.com>
2022-07-01 05:23:52 +0530

    first pushing js project

---
+++ other_logic_calculator/index.js
@@ -0,0 +1,47 @@
+let outputScreen = document.getElementById('output');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        // console.log("button text is ",buttonText);
+        if(buttonText == 'X'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value += screenValue;
+        }else if(buttonText=='C'){
+            screenValue = "";
+            screen.value = screenValue;
+            
+        }else if(buttonText == '='){
+            screen.value = eval(screenValue);
+
+        }else if(buttonText == '/'){
+            screenValue += buttonText;
+            screen.value += screenValue;
+
+        }else if(buttonText == '-'){
+            screenValue += buttonText;
+            screen.value += screenValue;
+
+
+        }else if(buttonText == '+'){
+            screenValue += buttonText;
+            screen.value += screenValue;
+        }
+        else{
+            screenValue +=buttonText;
+            screen.value = screenValue;
+        };
+
+    });
+
+};
+
+
+
+
+
+
+
